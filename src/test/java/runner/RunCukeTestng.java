afc5a34ead205da0318902630147abea5dafdbc3
Roopa <roopaworld@yahoo.com>
2020-05-13 21:51:08 +0100

    methods moved from navbarPage

--- src/test/java/runner/RunCukeTestng.java
+++ src/test/java/runner/RunCukeTestng.java
@@ -31,7 +31,7 @@ import utils.utilities;
        features = {"src/test/java/features/bookAppointment.feature"},
         glue = {"stepDefinitions"},
         monochrome = true,
-        tags = {"@BookByAppointmentFilters"},
+        tags = {"@smoke"},
 
 
 
